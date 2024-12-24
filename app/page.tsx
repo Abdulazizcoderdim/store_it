f304e821fe1af65358784f9b3e1ef93fe50c2b77
AbdulazizRus <coderdim@gmail.com>
2024-12-24 16:30:14 +0500

    auth layout page added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">Store - The only storage solution you need.</h1>
+    <div className="flex justify-center items-center h-screen bg-amber-100">
+      <h1 className="text-3xl font-bold text-brand">
+        StoreIt - The only storage solution you need.
+      </h1>
     </div>
   );
 }
