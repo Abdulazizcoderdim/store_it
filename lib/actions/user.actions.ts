cce0fc94da9d291f6ce62f13a40ebf1e95537355
AbdulazizRus <coderdim@gmail.com>
2025-02-22 16:17:49 +0500

    change

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,6 +6,7 @@ import { appwriteConfig } from '../appwrite/config';
 
 const getUserByEmail = async (email: string) => {
   const { databases } = await createAdminClient();
+  
 
   const result = await databases.listDocuments(
     appwriteConfig.databaseId,
