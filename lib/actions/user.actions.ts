40ee0610d7f9f4d4d95db525d2b54eb2f4aa3450
AbdulazizRus <coderdim@gmail.com>
2025-02-26 20:58:49 +0500

    changes

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,7 +6,6 @@ import { appwriteConfig } from '../appwrite/config';
 
 const getUserByEmail = async (email: string) => {
   const { databases } = await createAdminClient();
-  
 
   const result = await databases.listDocuments(
     appwriteConfig.databaseId,
@@ -44,4 +43,25 @@ const createAccount = async ({
   const existingUser = await getUserByEmail(email);
 
   const accountId = await sendEmailOTP({ email });
+
+  if (!accountId) throw new Error('Failed to send OTP');
+
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
+
+    await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        avatar:
+          'https://www.shareicon.net/data/128x128/2016/07/26/802043_man_512x512.png',
+        accountId,
+      }
+    );
+  }
+
+  return parseStringify({ accountId });
 };
