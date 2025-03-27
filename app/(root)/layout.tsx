22827eb0db4b656fdddd4aeea808acc0cbc488ba
AbdulazizRus <coderdim@gmail.com>
2025-03-27 15:15:12 +0500

    ui page started

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,17 @@
-const Layout = ({children}: {children: React.ReactNode}) => {
+import Header from "@/components/Header";
+import MobileNavigation from "@/components/MobileNavigation";
+import Sidebar from "@/components/Sidebar";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex h-screen">
-        Sidebar
-
-        <section className="flex h-full flex-1 flex-col">
-            Mobile Navigatation header
-            <div className="main-content">{children}</div>
-        </section>
+      <Sidebar />
+      <section className="flex h-full flex-1 flex-col">
+        <MobileNavigation /> <Header />
+        <div className="main-content">{children}</div>
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
