d97b5240e173e43cfdacbd480399c131348b6d85
AbdulazizRus <coderdim@gmail.com>
2025-05-11 07:55:19 +0500

    change

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,17 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      search
+      <div className="header-wrapper">
+        File Uploader
+        <form>
+          <Button>
+            <Image src="/assests/icons/logout.svg" alt="logo" />
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
